e2f39e2f900d4b209edf3895dee1e3942962a0bf
Jean Marte <jeanmarte93@gmail.com>
2024-07-16 10:58:39 -0400

    cdk

--- cdk_workshop/cdk_workshop_stack.py
+++ cdk_workshop/cdk_workshop_stack.py
@@ -1,26 +1,31 @@
 from constructs import Construct
 from aws_cdk import (
-    Duration,
     Stack,
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
 )
 
+from .hitcounter import HitCounter
 
 class CdkWorkshopStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "CdkWorkshopQueue",
-            visibility_timeout=Duration.seconds(300),
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_11,
+            code=_lambda.Code.from_asset('lambda'),
+            handler='hello.handler', 
         )
-
-        topic = sns.Topic(
-            self, "CdkWorkshopTopic"
+        
+        hello_with_hitcounter = HitCounter(
+            self, 'HelloHitCounter',
+            downstream=my_lambda,
         )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
+        
+        apigw.LambdaRestApi(
+            self, 'Endpoint',
+            handler=hello_with_hitcounter.handler,
+        )
+        
